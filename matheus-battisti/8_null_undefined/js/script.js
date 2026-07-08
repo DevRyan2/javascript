eea5157d925618a20aac09dc82c0d7a7f70b8613
RyanDev <ryancarlosamorimsouza@gmail.com>
2026-07-08 17:22:28 -0300

    criei o ex 6 ao 8

---
+++ matheus-battisti/8_null_undefined/js/script.js
@@ -0,0 +1,18 @@
+// Hoisting - içamento
+
+console.log(sobrenome)
+console.log(numero)
+
+var nome = null
+var sobrenome = 'Carlos'
+
+console.log(nome)
+console.log(sobrenome)
+
+nome = 'Ryan'
+
+console.log(nome)
+
+var numero = 5
+
+console.log(numero)
