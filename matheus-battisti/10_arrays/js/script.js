bd93a4122b26a6acb24615f5961e46d3a7c5c986
RyanDev <ryancarlosamorimsouza@gmail.com>
2026-07-09 19:39:45 -0300

    ex 9 e 10 do matheus

---
+++ matheus-battisti/10_arrays/js/script.js
@@ -0,0 +1,18 @@
+var arr = [5, 'Ryan', true, {teste: 1, teste: 2}]; // obj => {}, sempre começa com 0
+
+console.log(arr)
+
+var arr2 = [2, 3, 4, 5, 6]
+
+console.log(arr2)
+
+console.log(arr[1])
+console.log(arr[0])
+
+arr[4] = 'Carlos'
+
+arr[0] = 'Ryan 0'
+
+console.log(arr)
+
+console.log(typeof arr)
