6cf9017fe58f9cf8df408d237781cdfcd146ccb3
RyanDev <ryancarlosamorimsouza@gmail.com>
2026-07-16 10:00:46 -0300

    tipos de dados da origamid

---
+++ origamid/javascript-para-iniciantes/numeros-e-operadores/script.js
@@ -0,0 +1,32 @@
+// Declare uma variável contendo uma string
+
+var string = 'Essa é uma string';
+console.log(typeof string);
+
+// Declare uma variável contendo um número dentro de uma string
+
+var ano = '2006';
+console.log(typeof ano);
+
+// Declare uma variável com a sua idade
+
+var idade = 19;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+var nome = 'Ryan';
+var sobrenome = 'Carlos';
+var nomeCompleto = `${nome} ${sobrenome}`;
+console.log(nomeCompleto);
+
+// Coloque a seguinte frase em uma variável: It's time
+
+var frase1 = `It's time`;
+console.log(frase1);
+
+// Verifique o tipo da variável que contém o seu nome
+
+var verificarTipoNome = typeof nome;
+
+console.log(verificarTipoNome);
---
+++ origamid/javascript-para-iniciantes/tipos-de-dados/script-aula.js
@@ -0,0 +1,33 @@
+var nome = 'Ryan';
+
+var idade = 19;
+
+var simbolo = Symbol();
+
+console.log(typeof simbolo);
+
+var nome = 'Ryan';
+var sobrenome = 'Souza';
+var nomeCompleto = nome + ' ' + sobrenome;
+console.log(nomeCompleto);
+
+// var gols = 1000;
+// var frase = 'Romário fez ' + gols + ' gols';
+// console.log(typeof frase);
+
+var ano = 2006;
+var mes = 8;
+console.log(ano + mes);
+
+var melhor = 'Teste';
+var frase1 = 'Esse é o "melhor" jogo';
+console.log(frase1);
+
+console.log('Isso mesmo');
+
+var gols = 1000;
+var frase1 = 'Romário fez ' + gols + ' gols';
+var frase2 = `Romário fez ${gols * 2} gols`;
+
+console.log(frase1);
+console.log(frase2);
