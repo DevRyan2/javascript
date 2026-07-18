3e984646d4c44b96ad30fa11ba1755c237e11dda
RyanDev <ryancarlosamorimsouza@gmail.com>
2026-07-18 19:34:14 -0300

    boolean e condicionais 2 origamid

--- origamid/javascript-para-iniciantes/boolean-e-condicionais/script.js
+++ origamid/javascript-para-iniciantes/boolean-e-condicionais/script.js
@@ -1,29 +1,55 @@
-var possuiGraduacao = false;
-var possuiDoutorado = true;
+// Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
 
-if (possuiGraduacao) {
-    console.log('É verdadeiro');
-} else if (possuiDoutorado) {
-    console.log('Possui Doutorado');
+var minhaIdade = 18;
+var idadePrimo = 17;
+
+if (minhaIdade > idadePrimo) {
+    console.log(`É maior`);
+} else if (minhaIdade < idadePrimo) {
+    console.log(`É menor`);
+} else if (minhaIdade == idadePrimo) {
+    console.log(`É igual`);
 } else {
-    console.log('Não possui nada');
+    console.log(`Valor inválido`);
 }
 
-var nome = '10kg' / 10;
+// Qual valor é retornado na seguinte expressão?
+var expressao = 5 - 2 && 5 - ' ' && 5 - 2;
+console.log(expressao);
+// true
 
-if (nome) {
-    console.log(nome);
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var nome = 'Andre'; //true
+var idade = 28; //true
+var possuiDoutorado = false; //false
+var empregoFuturo; //false
+var dinheiroNaConta = 0; //false
+
+console.log(!!nome, !!idade, !!possuiDoutorado, !!empregoFuturo, !!dinheiroNaConta)
+
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+
+if (brasil > china) {
+    console.log(`O brasil tem mais habitantes`)
+} else if (china == brasil){
+    console.log(`O brasil é a china tem a mesma quantidade de habitantes`)
 } else {
-    console.log('Nome não existe');
+    console.log(`A china tem mais habitantes`)
 }
 
-if (!possuiGraduacao) {
-    console.log(possuiGraduacao);
-    console.log('Possui graduação');
+// O que irá aparecer no console?
+if ('Gato' === 'gato' && 5 > 2) {
+    console.log('Verdadeiro');
 } else {
-    console.log('Não possui graduação');
+    console.log('Falso');
 }
 
-var x = '10';
-
-console.log(x !== 10);
+// O que irá aparecer no console?
+if ('Gato' === 'gato' || 5 > 2) {
+    console.log('Gato' && 'Cão');
+} else {
+    console.log('Falso');
+}
