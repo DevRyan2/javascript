15e5112561702d2f702b7e2f2f075471bd1aae3e
RyanDev <ryancarlosamorimsouza@gmail.com>
2026-07-21 22:06:08 -0300

    aula de repeticao 1 do guanabara

---
+++ gustavo-guanabara/aulas/aula13/ambiente.js
@@ -0,0 +1,29 @@
+var c = 1;
+
+do {
+    // c += 1
+    // console.log(c)
+    console.log(`Passo ${c}`);
+    c++;
+} while (c <= 6);
+
+/*
+while (c <= 5) {
+    // c += 1
+    // console.log(c)
+    console.log(`Passo ${c}`)
+    c++
+}
+
+*/
+
+/*
+
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+
+*/
