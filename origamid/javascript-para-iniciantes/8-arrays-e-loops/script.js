1c30abdb48b55788cc9e0757b40c37b9cc80dc49
RyanDev <ryancarlosamorimsouza@gmail.com>
2026-07-27 22:37:38 -0300

    fiz a aula 1 de arrays e loops da origamid

---
+++ origamid/javascript-para-iniciantes/8-arrays-e-loops/script.js
@@ -0,0 +1,21 @@
+var videoGames = ['Switch', 'PS4', 'XBOX'];
+
+var ultimoItem = videoGames.pop(); // remove o ultimo item
+
+// =================================================
+
+for (var numero = 1; numero <= 10; numero++) {
+    console.log(numero);
+}
+
+// =================================================
+
+var i = 0;
+
+while (i < 10) {
+    console.log(i);
+    i += 1;
+}
+
+// =================================================
+
