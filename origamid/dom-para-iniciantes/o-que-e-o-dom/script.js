d0cb1af8cf3d94b86b578c22ec5ab3433fd11796
RyanDev <ryancarlosamorimsouza@gmail.com>
2026-07-28 19:23:16 -0300

    fiz a aula 1 de o que é o dom da origamid

---
+++ origamid/dom-para-iniciantes/o-que-e-o-dom/script.js
@@ -0,0 +1,21 @@
+// window.alert('Isso mesmo');
+
+const href = window.location.href;
+
+console.log(href);
+
+// ===============================================
+
+if (
+    href === 'http://127.0.0.1:5500/origamid/dom-para-iniciantes/o-que-e-o-dom/'
+) {
+    console.log('É igual');
+}
+
+// ===============================================
+
+const h1Selecionado = document.querySelector('h1');
+
+// ===============================================
+
+// alert('Teste')
