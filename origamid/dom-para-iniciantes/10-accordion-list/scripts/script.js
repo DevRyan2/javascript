f74163084c06b766c057bc7881d79f3f36f81aa1
RyanDev <ryancarlosamorimsouza@gmail.com>
2026-08-04 15:00:26 -0300

    accordion list / origamid

---
+++ origamid/dom-para-iniciantes/10-accordion-list/scripts/script.js
@@ -0,0 +1,53 @@
+function initTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+    if (tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('ativo');
+
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            });
+
+            tabContent[index].classList.add('ativo');
+        }
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
+        });
+    }
+}
+
+initTabNav();
+
+function initAccordion() {
+
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const primeiroItem = accordionList[0]
+    const activeClass = 'ativo'
+
+    if(accordionList.length) {
+
+        primeiroItem.classList.add(activeClass)
+        primeiroItem.nextElementSibling.classList.add(activeClass)
+
+        function activeAccordion() {
+
+            this.classList.toggle(activeClass) // adiciona a class ativo ao dt
+            this.nextElementSibling.classList.toggle(activeClass) // adiciona a class ativo ao dd
+
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        });
+
+    }
+
+}
+
+initAccordion()
