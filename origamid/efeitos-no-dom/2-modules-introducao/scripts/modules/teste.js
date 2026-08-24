3ff4ce5123bfd8698e55e1c65efcd76e353b66d2
RyanDev <ryancarlosamorimsouza@gmail.com>
2026-08-24 09:44:33 -0300

    modules 1 / origamid

---
+++ origamid/efeitos-no-dom/2-modules-introducao/scripts/modules/teste.js
@@ -0,0 +1,7 @@
+export function testando() {
+    console.log('isso é teste')
+}
+
+export function teste2() {
+    console.log('Teste 2')
+}
---
+++ origamid/efeitos-no-dom/2-modules-introducao/scripts/script.js
@@ -0,0 +1,14 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimationScroll from './modules/animacao-scroll.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import * as teste from './modules/teste.js';
+// import {testando as teste, teste2} from './modules/teste.js'
+
+teste.testando();
+teste.teste2();
+
+initScrollSuave(); // scroll suave
+initAnimationScroll(); // animacao do scroll
+initAccordion(); // accordion
+initTabNav();
