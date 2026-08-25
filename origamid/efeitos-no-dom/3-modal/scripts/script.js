ab20e82c6cc49d1089866a25dbbdb2084850bced
RyanDev <ryancarlosamorimsouza@gmail.com>
2026-08-25 17:28:00 -0300

    modal 2 / origamid

--- origamid/efeitos-no-dom/3-modal/scripts/script.js
+++ origamid/efeitos-no-dom/3-modal/scripts/script.js
@@ -2,8 +2,10 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimationScroll from './modules/animacao-scroll.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
 
 initScrollSuave(); // scroll suave
 initAnimationScroll(); // animacao do scroll
 initAccordion(); // accordion
 initTabNav();
+initModal();
