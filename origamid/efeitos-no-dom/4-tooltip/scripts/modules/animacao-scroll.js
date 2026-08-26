d6d25eef36837bd44b49672dc8c355209e647bc5
RyanDev <ryancarlosamorimsouza@gmail.com>
2026-08-26 09:45:45 -0300

    tooltip / origamid

---
+++ origamid/efeitos-no-dom/4-tooltip/scripts/modules/animacao-scroll.js
@@ -0,0 +1,28 @@
+export default function initAnimationScroll() {
+    const sections = document.querySelectorAll('[data-anime="scroll"]');
+
+    // sections[0].classList.add('ativo')
+
+    if (sections.length) {
+        function animaScroll() {
+            const windowMetade = window.innerHeight * 0.5;
+
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = sectionTop - windowMetade < 0;
+
+                if (isSectionVisible) {
+                    section.classList.add('ativo');
+                } // else {
+
+                //     section.classList.remove('ativo')
+
+                // }
+            });
+        }
+
+        animaScroll();
+
+        window.addEventListener('scroll', animaScroll);
+    }
+}
---
+++ origamid/efeitos-no-dom/4-tooltip/scripts/script.js
@@ -0,0 +1,13 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimationScroll from './modules/animacao-scroll.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initToolTip from './modules/tooltip.js';
+
+initScrollSuave(); // scroll suave
+initAnimationScroll(); // animacao do scroll
+initAccordion(); // accordion
+initTabNav();
+initModal();
+initToolTip();
