df64d3ac0c2858d2ad22334099b023c1db0db307
RyanDev <ryancarlosamorimsouza@gmail.com>
2026-08-27 13:56:25 -0300

    dropdown-menu / origamid

---
+++ origamid/efeitos-no-dom/5-dropdown-menu/scripts/script.js
@@ -0,0 +1,15 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimationScroll from './modules/animacao-scroll.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initToolTip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+
+initScrollSuave(); // scroll suave
+initAnimationScroll(); // animacao do scroll
+initAccordion(); // accordion
+initTabNav();
+initModal();
+initToolTip();
+initDropdownMenu();
